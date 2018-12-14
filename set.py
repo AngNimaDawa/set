8882441b35e5de7a2ddc400eb2374ec72597449e
AngNimaDawa <45613061+AngNimaDawa@users.noreply.github.com>
2018-12-14 08:40:33 +0545

    Create set.py

---
+++ set.py
@@ -0,0 +1,37 @@
+ #set
+ #set is onorder
+ #{} element seprated by commmoa
+# basket1 = ['apple','orange','apple','pear','orange','banana']
+# fruits = set(basket1)
+# print(fruits)
+
+
+# basket2 = ['apple','orange','apple','pear','orange','banana','apple','banana','apple','orange','pear']
+# for fruit in set(basket2):
+#  print((fruit,basket2.count(fruit)))
+# result.append((fruit, basket2.count(fruit)))
+
+
+
+basket1 = ['apples','orange','pear','papaya','banana','tomato']
+basket2 = ['patato','brinja','tomato','banana','papaya']
+fruits = set(basket1)
+vegitables = set(basket2)
+# print(fruits)
+# print(veaitables)
+
+
+
+#intersection
+# print('common or intersection:{}'.format(fruits & vegitables))
+
+
+#only fruits 
+# print('only fruits:{}'.format(fruits - vegitables))
+
+
+#union
+
+# print('union: {}'.format(fruits | vegitables))
+# fruits.update(vegitables)
+# print(fruits)
